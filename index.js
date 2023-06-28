1f95db3f7afc805b4aa15789c519584f4c0e9d88
Miguel Negron Garcia <josuefrontend@gmail.com>
2023-06-28 13:19:23 -0400

    Agregue el gitignore y los primeros archivos del proyecto

---
+++ index.js
@@ -0,0 +1,20 @@
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const form = document.querySelector('#form');
+const calcularButton = document.querySelector('#btnCalcula');
+const resultadoElement = document.querySelector('#resultado');
+
+form.addEventListener('submit', verifyingData)
+
+function verifyingData(e) {
+    e.preventDefault();
+
+    if(!isNaN(input1.value && input2.value)){
+        let totaFromInput = Number(input1.value) + Number(input2.value);
+        resultadoElement.innerHTML = "El resultado es " + totaFromInput;
+
+    }else{            
+        resultadoElement.innerHTML = 'Lo sentimos, solo aceptamos numeros, verifique los datos que ingreso y asegurece que no halla ninguna letra. ';
+        }
+        
+    }
